67b38072f7c2f659e3dd5394dfe2b60676649e14
Adam Ripley <aripley@wharton.upenn.edu>
2018-01-27 14:42:53 -0500

    nodemon and routes setup

--- routes.js
+++ routes.js
@@ -4,7 +4,37 @@ const router = new express.Router();
 router.get('/', (req, res) => {
   res.send('Welcome to the PennClubReview API!');
 });
-  
+
+// Route to list all the clubs
+// TODO implement
+router.get('/clubs', (req, res) => {
+  res.send('List of clubs!');
+});
+
+// Route to create a new club
+// TODO implement
+router.post('/clubs', (req, res) => {
+  res.send('Create a new club!.');
+});
+
+// Route to change the ranking of a specified club
+// TODO implement
+router.post('/rankings', (req, res) => {
+  res.send('Change rankings.')
+});
+
+// Route to list the rankings of the clubs
+// TODO implement
+router.get('/rankings', (req, res) => {
+  res.send('List of rankings.');
+});
+
+// Route to return a given user
+// TODO implement
+router.get('/user/:id', (req, res) => {
+  res.send('Given user.');
+});
+
 router.post('/', (req, res) => {
   res.send('The request body is: ' + req.body);
 });
