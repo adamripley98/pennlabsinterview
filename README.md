fab88b32adf37420a2ba6e72f314fe263496e06b
Adam Ripley <aripley@wharton.upenn.edu>
2018-01-28 23:29:59 -0500

    readme

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Penn Server Challenge
+# Penn Server Challenge - 2018 PennLabs Takehome Challenge
 
-## Installation
-1. Clone repo.
-2. Install packages using `npm install`.
-3. Run `node index.js` to run project.
-4. Follow the instructions [here](https://www.notion.so/pennlabs/Server-Challenge-Spring-057cb31ac6744f7696a4860a8f861e50).
-5. Document your work in a new file named `DOCUMENTATION.MD`.
-6. Send us a github link.
+__Technology used:__
+* `Node` JavaScript on the backend
+* `Express` backend API library
+* `MongoDB` database
+* `Handlebars` Templating engine
+* `BCrypt` Secure password storing
+
+To run the application, set up an `env.sh` connecting to Mongo. Run `npm install`,`source env.sh` and `nodemon index.js`.
+
+# API
+```javascript
+/* Frontend */
+GET '/'                        // Route to add users, add clubs, and change rankings
+
+/* API */
+GET '/api/'                    // Ensure API is working
+GET '/api/clubs'               // Returns a list of all clubs
+POST '/api/clubs'              // Create a new club
+GET '/api/rankings'            // Get Jennifer's rankings
+POST '/api/rankings'           // Change Jennifer's ranking
+GET '/api/user/:id'            // Return a given user's information (except private information)
+POST '/api/user/new'           // Create a new user
+GET '/api/users'               // Return a list of all the registered users
+```
+# Notes
+* Passwords are stored securely using BCrypt and MongoDB.
+* Instead of storing the user and clubs in a JSON file, I stored them in MongoDB. Note: I did originally implement the required functionality using JSON files and you may go back in my commit history if interested in that code.
+* Error checking is done to ensure fields are not empty, users are not registering with duplicate usernames, duplicate clubs aren't allowed, and users can't rank clubs with numbers outside of a given range.
+* Next feature I was planning to implement was `Passport.js` secure local login and register. However I did not get to this. While you can create users in Mongo currently, there is nothing happening with the sessions indicating any sort of user login.
+* The `env.sh` file contains a `MONGODB_URI` to connect to MongoDB, as well as a `jennId` to store the value of Jennifer's id as created by Mongo. For now, the routes use `jennId` instead of a user's id stored in sessions. I will send a copy of my `env.sh` file via email with my submission.
+* Code is commented thoroughly throughout to explain functionality.
+* Additional features implemented beyond required: functionality to add users, ability to display all users, database storage of users and clubs, and a rough beginning of a frontend using handlebars.
--- routes.js
+++ routes.js
@@ -10,12 +10,14 @@ const User = require('./models/user');
 // Import Jennifer's id from env.sh file
 const jennId = process.env.jennId;
 
+// Route to ensure API is working
 router.get('/', (req, res) => {
   res.send({success: true, data: 'Welcome to the PennClubReview API!'});
 });
 
 // Route to list all the clubs
 router.get('/clubs', (req, res) => {
+  // Find all clubs in Mongo
   Club.find({}, (err, clubs) => {
     if (err) {
       res.send({success: false, error: err.message});
@@ -27,50 +29,70 @@ router.get('/clubs', (req, res) => {
 
 // Route to create a new club
 router.post('/clubs', (req, res) => {
-  // Attempt to find club in Mongo, can't create duplicate club
-  Club.find({
-    name: req.body.name
-  }, (err, club) => {
-    if (err) {
-      res.send({success: false, error: err.message});
-      // If club already exists, return error
-    } else if (club.length) {
-      res.send({
-        success: false,
-        error: 'A club with the name ' + req.body.name + ' already exists.'
-      });
-    } else {
-      // Declare new club
-      const newClub = new Club({name: req.body.name, size: req.body.size});
-      // Save in Mongo
-      newClub.save((err) => {
-        if (err) {
-          res.send({success: false, error: err.message});
-        } else {
-          User.findById(jennId, (errUser, user) => {
-            if (errUser) {
-              res.send({success: false, error: errUser.message});
-            } else {
-              // TODO save rankings better in mongo
-              const currentRankings = user.rankings;
-              currentRankings.push(newClub);
-              user.rankings = currentRankings;
-              user.save((errSave) => {
-                if (errSave) {
-                  res.send({success: false, error: errSave.message});
-                } else {
-                  res.send({
-                    success: true,
-                    data: 'New club ' + newClub.name + " created!"
-                  });
+  // Error checking for empty name
+  if (!req.body.name) {
+    res.send({
+      success: false,
+      error: 'Name cannot be blank.',
+    });
+  // Error checking for empty size
+  } else if (!req.body.size) {
+    res.send({
+      success: false,
+      error: 'Size cannot be blank.',
+    });
+  } else {
+    // Attempt to find club in Mongo, can't create duplicate club
+    Club.find({
+      name: req.body.name
+    }, (err, club) => {
+      if (err) {
+        res.send({success: false, error: err.message});
+        // If club already exists, return error
+      } else if (club.length) {
+        res.send({
+          success: false,
+          error: 'A club with the name ' + req.body.name + ' already exists.'
+        });
+      } else {
+        // Declare new club
+        const newClub = new Club({name: req.body.name, size: req.body.size});
+        // Save in Mongo
+        newClub.save((err) => {
+          if (err) {
+            res.send({success: false, error: err.message});
+          } else {
+            // Find jennifer in mongo
+            User.findById(jennId, (errUser, user) => {
+              if (errUser) {
+                res.send({success: false, error: errUser.message});
+              } else {
+                // Update user's rankings with new club
+                const currentRankings = user.rankings;
+                newRank = {
+                  name: newClub.name,
+                  ranking: currentRankings.length + 1,
                 }
-              });
-            }
-          });
-        }
-      });
-    }
-  });
+                currentRankings.push(newRank);
+                user.rankings = currentRankings;
+                // Save changes to user in Mongo
+                user.save((errSave) => {
+                  if (errSave) {
+                    res.send({success: false, error: errSave.message});
+                  } else {
+                    res.send({
+                      success: true,
+                      data: 'New club ' + newClub.name + " created!"
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
 });
 
 // Route to change the ranking of a specified club
@@ -80,55 +102,70 @@ router.post('/rankings', (req, res) => {
   const clubRanking = req.body.ranking;
   const name = req.body.name;
 
-  // Find Jenn in mongo
-  User.findById(jennId, (err, user) => {
-    if (err) {
-      res.send({success: false, error: err.message});
-    } else {
-      const clubs = user.rankings;
-      // Error check for proper ranking entering
-      if (clubRanking < 1 || clubRanking > clubs.length) {
-        res.send({
-          success: false,
-          error: 'Ranking must be in range ' + 1 + '-' + clubs.length
-        })
-        // Error checking to ensure club name entered is in list
+  // Error checking for empty ranking
+  if (!clubRanking) {
+    res.send({
+      success: false,
+      error: 'Ranking cannot be empty.',
+    });
+  // Error checking for empty name
+  } else if (!name) {
+    res.send({
+      success: false,
+      error: 'Name cannot be empty.',
+    });
+  } else {
+    // Find Jenn in mongo
+    User.findById(jennId, (err, user) => {
+      if (err) {
+        res.send({success: false, error: err.message});
       } else {
-        let isPresent = false;
-        let oldIndex = -1;
-        for (var i = 0; i < clubs.length; i++) {
-          // To ignore cases
-          if (clubs[i].name.toLowerCase() === name.toLowerCase()) {
-            isPresent = true;
-            oldIndex = i;
-          }
-        }
-        if (!isPresent) {
+        const clubs = user.rankings;
+        // Error check for proper ranking entering
+        if (clubRanking < 1 || clubRanking > clubs.length) {
           res.send({
             success: false,
-            error: 'Club ' + name + ' is not on the list.'
-          });
+            error: 'Ranking must be in range ' + 1 + '-' + clubs.length
+          })
+          // Error checking to ensure club name entered is in list
         } else {
-          const newIndex = clubRanking - 1;
-          // Updates the position of the club
-          clubs.splice(newIndex, 0, clubs.splice(oldIndex, 1)[0]);
-          // Update the order of the user's ranking
-          user.rankings = clubs;
-          // Save changes in Mongo
-          user.save((err) => {
-            if (err) {
-              res.send({success: false, error: err.message});
-            } else {
-              res.send({
-                success: true,
-                data: name + ' is now ranked #' + clubRanking + "!"
-              });
+          let isPresent = false;
+          let oldIndex = -1;
+          for (var i = 0; i < clubs.length; i++) {
+            // To ignore upper/lower case
+            if (clubs[i].name.toLowerCase() === name.toLowerCase()) {
+              isPresent = true;
+              oldIndex = i;
             }
-          });
+          }
+          // If club doesn't exist, cannot update ranking
+          if (!isPresent) {
+            res.send({
+              success: false,
+              error: 'Club ' + name + ' is not on the list.',
+            });
+          } else {
+            const newIndex = clubRanking - 1;
+            // Updates the position of the club
+            clubs.splice(newIndex, 0, clubs.splice(oldIndex, 1)[0]);
+            // Update the order of the user's ranking
+            user.rankings = clubs;
+            // Save changes in Mongo
+            user.save((err) => {
+              if (err) {
+                res.send({success: false, error: err.message});
+              } else {
+                res.send({
+                  success: true,
+                  data: name + ' is now ranked #' + clubRanking + "!",
+                });
+              }
+            });
+          }
         }
       }
-    }
-  });
+    });
+  }
 });
 
 // Route to list the rankings of the clubs
@@ -144,7 +181,7 @@ router.get('/rankings', (req, res) => {
       for (var i = 0; i < clubs.length; i++) {
         rankings.push({
           "ranking": i + 1,
-          "club": clubs[i].name
+          "club": clubs[i].name,
         });
       }
       // Send back rankings
@@ -178,40 +215,101 @@ router.get('/user/:id', (req, res) => {
 
 // Route to create a new user
 router.post('/user/new', (req, res) => {
-  console.log(req.body);
   // Generates hash using bCrypt, storing password safely
   const createHash = (password) => {
     return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
   };
+  // Error checking: username, name, and password cannot be empty
+  if (!req.body.username) {
+    res.send({
+      success: false,
+      error: 'Username cannot be empty',
+    });
+  } else if (!req.body.name) {
+    res.send({
+      success: false,
+      error: 'Name cannot be empty',
+    });
+  } else if (!req.body.password) {
+    res.send({
+      success: false,
+      error: 'Password cannot be empty',
+    });
+  } else {
+    // Check to make sure a user with given username doesn't already exist
+    User.find({username: req.body.username}, (errUser, user) => {
+      if (errUser) {
+        res.send({
+          success: false,
+          error: errUser.message,
+        });
+        // Error check: user already exists
+      } else if (user.length) {
+        res.send({
+          success: false,
+          error: 'User with username ' + req.body.username + ' already exists.',
+        });
+      } else {
+        // Find all clubs to add to user
+        Club.find({}, (errClub, clubs) => {
+          if (errClub) {
+            res.send({success: false, error: errClub});
+          } else {
+            const rankings = [];
+            // Filter unnecessary club information stored in user
+            for (var i = 0; i < clubs.length; i++) {
+              rankings.push({
+                name: clubs[i].name,
+                ranking: i + 1,
+              });
+            }
+            // Create a new user with given params
+            // NOTE password is hashed to store securely
+            // NOTE rankings are default the order in which they are in Mongo
+            const newUser = new User({
+              name: req.body.name,
+              username: req.body.username,
+              password: createHash(req.body.password),
+              rankings: rankings,
+            });
+            // Save new user in Mongo
+            newUser.save((err, user) => {
+              if (err) {
+                res.send({success: false, error: err.message});
+              } else {
+                res.send({
+                  success: true,
+                  data: 'New user ' + req.body.name + ' created. Access his/her information at the route: api/user/' + user._id,
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
 
-  // Find all clubs to add to user
-  Club.find({}, (errClub, clubs) => {
-    if (errClub) {
-      res.send({success: false, error: errClub});
-    } else {
-      // Create a new user with given params
-      // NOTE password is hashed to store securely
-      // NOTE rankings are default the order in which they are in Mongo
-      // TODO display clubs better, only pass back some data
-      const newUser = new User({
-        fullname: req.body.fullname,
-        username: req.body.username,
-        password: createHash(req.body.password),
-        rankings: clubs
+// Display all users who are registered
+router.get('/users', (req, res) => {
+  // Find all users in Mongo
+  User.find({}, (err, u) => {
+    if (err) {
+      res.send({
+        success: false,
+        error: err.message,
       });
-      // Save new user in Mongo
-      newUser.save((err, user) => {
-        if (err) {
-          res.send({success: false, error: err.message});
-        } else {
-          res.send({
-            success: true,
-            data: 'New user ' + req.body.fullname + ' created. Access his/her information at the route: api/user/' + user._id,
-          });
-        }
+    } else {
+      const users = [];
+      u.forEach((user) => {
+        users.push({name: user.name, userId: user._id});
+      });
+      res.send({
+        success: true,
+        data: users,
       });
     }
-  });
+  })
 });
 
 router.post('/', (req, res) => {
